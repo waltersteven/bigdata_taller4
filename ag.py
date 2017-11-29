ad2ac955857456e5e357e5060e9faa02fb8c0bde
Ivan Palomares <shades1017@gmail.com>
2017-11-29 13:03:40 -0500

    ag.py updated

--- ag.py
+++ ag.py
@@ -1,20 +1,21 @@
 import random
 import string
+from pyspark import SparkContext, SparkConf
+from pyspark.sql import SparkSession
 
-CADENA_A_BUSCAR = 'CAMILO'
 
 class ADN:
     def __init__(self, generador, fitness, reproduccion, mutacion, porcentaje_mutacion):
         self.generador = generador
         self.fitness = fitness
-        self.genes = ""
+        self.genes = []
         self.fitness_result = 0
         self.reproduccion = reproduccion
         self.mutacion = mutacion
         self.porcentaje_mutacion = porcentaje_mutacion
 
-    def generar(self, longitud):
-        self.genes = self.generador(longitud)
+    def generar(self):
+        self.genes = self.generador()
 
     def calcular_fitness(self):
         self.fitness_result = self.fitness(self.genes)
@@ -34,9 +35,9 @@ class ADN:
             #pos = random.randint(0, len(lista)-1)
             #lista[pos] = random.choice(string.ascii_uppercase)
             #self.genes = "".join(lista)
-    
+
     def __str__(self):
-        return " ".join(self.genes)
+        return " ".join(str(e) for e in self.genes)
 
 class Poblacion:
     def __init__(self, cantidad, generador, fitness, f_reproductora, f_mutadora, porcentaje_mutacion):
@@ -53,7 +54,7 @@ class Poblacion:
 
         for i in range(1,cantidad):
             especie = ADN(self.generador,self.fitness, self.f_reproductora, self.f_mutadora, self.porcentaje_mutacion)
-            especie.generar(len(CADENA_A_BUSCAR))
+            especie.generar()
             self.poblacion.append(especie)
             fitness_especie = especie.calcular_fitness()
             self.fitness_total = self.fitness_total + fitness_especie
@@ -93,18 +94,22 @@ class Poblacion:
             print("{} {}".format(especie, especie.calcular_fitness()))
 
 
-def generador(max):
-    cadena = ""
-    for i in range(max):
-        cadena = cadena + random.choice(string.ascii_uppercase)
+def generador():
+    cadena = [random.randint(0, 1), random.randint(0, 1), random.randint(0, 1),
+              random.randint(0, 1), random.randint(0, 1), random.randint(0, 1),
+              random.randint(0, 1), random.randint(0, 1), random.randint(0, 1),
+              random.randint(0, 1), random.randint(0, 1), random.randint(0, 1),
+              random.randint(0, 1), random.randint(0, 1), random.randint(0, 1),
+              random.randint(0, 1), random.randint(0, 1), random.randint(0, 1),
+              random.randint(0, 1), random.randint(0, 1), random.randint(0, 1),
+              random.randint(0, 1), random.randint(0, 1), random.randint(0, 1),
+              random.randint(0, 1), random.randint(0, 1), random.randint(0, 1),
+              random.randint(0, 1), random.randint(0, 1), random.randint(0, 1),
+              random.randint(0, 1), random.randint(0, 1)]
     return cadena
 
 def fitness(cadena):
-    cont = 0
-    for i in range(0,len(cadena)):
-        if cadena[i] == CADENA_A_BUSCAR[i]:
-            cont = cont + 1
-    return cont
+    return execute_logistic_regression_binomial(sc, "data/dataConRating1.csv", get_spark())
 
 def f_reproduccion(pareja1, pareja2):
     k = random.randint(0, len(pareja1.genes))
@@ -113,14 +118,14 @@ def f_reproduccion(pareja1, pareja2):
     return parte_izq + parte_der
 
 def f_mutacion(genes):
-    lista = list(genes)
+    lista = genes
     pos = random.randint(0, len(lista)-1)
-    lista[pos] = random.choice(string.ascii_uppercase)
-    return  "".join(lista)
+    lista[pos] = random.randint(0, 1)
+    return lista
 
 def main():
-    POBLACION = 100
-    MAX_ITERACIONES = 5000
+    POBLACION = 50
+    MAX_ITERACIONES = 500
     PORCENTAJE_MUTACION = 0.01
     poblacion = Poblacion(POBLACION, generador, fitness, f_reproduccion, f_mutacion, PORCENTAJE_MUTACION)
     for i in range(0,MAX_ITERACIONES):
@@ -130,7 +135,12 @@ def main():
         poblacion.reproduccion()
         print("Promedio Fitness: {}".format(poblacion.promedio_fitness()))
         poblacion.mutar()
-    
+
+def get_spark(self):
+    conf = SparkConf().setAppName('fifaModel').setMaster('local')
+    sc = SparkContext(conf=conf)
+    spark = SparkSession(sc)
+
 
 if __name__ == '__main__':
     main()
